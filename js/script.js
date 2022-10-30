5a0ba6b1be89a67de31d24acd7825a1673a0b941
Kamil Kuc <kamil.kuc95@gmail.com>
2022-10-30 10:03:50 +0100

    Add 2nd improved version on game

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Wygrałem!');
+
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+  playerMove = 'nożyce';
+}
+printMessage('Twój ruch: ' + playerMove);
